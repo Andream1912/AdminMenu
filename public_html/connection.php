b025dc674a131bcf49507d0baca1a6d8a49b54b3
Andrea <69152790+Andream1912@users.noreply.github.com>
2023-03-12 18:58:36 +0100

    l

--- public_html/connection.php
+++ public_html/connection.php
@@ -1,9 +1,9 @@
 <?php
 //DB CREDENTENSIAL
-define('DB_HOST', 'localhost');
-define('DB_USER', 'id20289518_unicabio');
-define('DB_PASS', 'Ciaociao2023_');
-define('DB_NAME', 'id20289518_unica');
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
 try {
     $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 } catch (PDOException $e) {
